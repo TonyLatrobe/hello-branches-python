6d7cbf4a400bc433d34fb2659bd2c944342d11ca
Tony Latrobe <a.desouza-daw@latrobe.edu.au>
2026-02-05 00:45:59 +0000

    Add excited hello option

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-def say_hello(name: str) -> str:
-    return f"Hello {name}"
+def say_hello(name: str, excited: bool = False) -> str:
+    greeting = f"Hello {name}"
+    return greeting + "!" if excited else greeting
 
-
+    
 if __name__ == "__main__":
-    print(say_hello("World"))
+    print(say_hello("Alice", excited=True))
